3a23c72817b8b67c6a082d95ae690565575d9c59
Andy McEniry <andy@mceniry.me.uk>
2016-11-19 22:45:47 +0000

    signInWithPopup

--- www/js/app.js
+++ www/js/app.js
@@ -41,8 +41,32 @@ var oApp = oApp || {};
     $('#signInWithGoogle').click(function() {
         console.log('signInWithGoogle');
         var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithRedirect(provider);
+        //firebase.auth().signInWithRedirect(provider);
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            console.log(result);
+            if (result.credential) {
+                var token = result.credential.accessToken;
+                $('body').append('<img src="' + result.user.photoURL + '" style="width:200px;margin:10px;">');
+                $('body').append('<p>' + result.user.displayName + '</p>');
+            }
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                // ...
+            }).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+
     });
+
 
     oApp.checkAuth = function () {
         console.log('checkAuth');
@@ -66,5 +90,6 @@ var oApp = oApp || {};
         });
 
     };
+
 
 }());
