3f91b5957bdf4a8b1635f6fec7c16e449fb6b474
Andy McEniry <andy@mceniry.me.uk>
2016-11-20 09:56:55 +0000

    firebase auth testing

--- www/js/app.js
+++ www/js/app.js
@@ -15,11 +15,12 @@ var oApp = oApp || {};
             console.log('no phonegap');
         } else {
             oApp.initLogger();
+            $('#log').removeClass('hide');
             console.log('phonegap loaded');
         }
         
         oApp.initFirebase();
-        //oApp.checkAuth();
+        oApp.checkAuth();
 
     };
 
@@ -47,37 +48,31 @@ var oApp = oApp || {};
         var provider = new firebase.auth.GoogleAuthProvider();
         console.log(provider);
 
-        firebase.auth().signInWithPopup(provider).then(function (result) {
-            console.log(result);
-            if (result.credential) {
-                var token = result.credential.accessToken;
-                $('body').append('<img src="' + result.user.photoURL + '" style="width:200px;margin:10px;">');
-                $('body').append('<p>' + result.user.displayName + '</p>');
-            }
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                var token = result.credential.accessToken;
-                // The signed-in user info.
-                var user = result.user;
-                // ...
-            }).catch(function(error) {
-                console.log(error.message);
-                console.log(error);
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                // ...
-            });
+        // firebase.auth().signInWithPopup(provider).then(function (result) {
+            // console.log(result);
+            // if (result.credential) {
+                // var token = result.credential.accessToken;
+                // $('body').append('<img src="' + result.user.photoURL + '" style="width:200px;margin:10px;">');
+                // $('body').append('<p>' + result.user.displayName + '</p>');
+            // }
+            // var token = result.credential.accessToken;
+            // var user = result.user;
+        // }).catch(function(error) {
+            // console.log(error.message);
+            // console.log(error);
+
+        // });
+
+        firebase.auth().signInWithRedirect(provider);
+
+
 
     });
 
 
     oApp.checkAuth = function () {
         console.log('checkAuth');
-        firebase.auth().getRedirectResult().then(function (result) {
+        firebase.auth().getRedirectResult().then(function(result) {
             console.log(result);
             if (result.credential) {
                 var token = result.credential.accessToken;
@@ -85,15 +80,9 @@ var oApp = oApp || {};
                 $('body').append('<p>' + result.user.displayName + '</p>');
             }
             var user = result.user;
-        }).catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
+        }).catch(function(error) {
+            console.log(error.message);
+            console.log(error);
         });
 
     };
